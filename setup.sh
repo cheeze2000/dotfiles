e412b18b840b8018b7274fef997fb70389ffe0cf
cheeze2000 <me@cheeze2000.net>
2022-10-09 21:53:13 +0800

    refactor: skip if symlink already exists

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 # Kitty
-ln -s ~/dotfiles/kitty ~/.config/kitty
-echo "Created a symlink to ~/.config/kitty"
+if [ -d ~/.config/kitty ]; then
+	echo "~/.config/kitty already exists, skipping..."
+else
+	ln -s ~/dotfiles/kitty ~/.config/kitty
+	echo "Created a symlink to ~/.config/kitty"
+fi
 
 echo "Cleaning up..."
 # rm ./setup.sh
