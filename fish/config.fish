adf1f2d3aed731730960bb8cf6ff25329817f9af
cheeze2000 <me@cheeze2000.net>
2022-10-09 22:54:51 +0800

    feat: fish config

---
+++ fish/config.fish
@@ -0,0 +1,29 @@
+function fish_prompt
+    set -l last_status $status
+    echo ""
+
+    if test $last_status -eq 0
+        set_color black -b yellow
+        echo -n " $(dirs) "
+        set_color yellow -b normal
+        echo -n ""
+    else
+        set_color --bold white -b red
+        echo -n " ! "
+
+        set_color normal
+
+        set_color black -b yellow
+        echo -n " $(dirs) "
+        set_color yellow -b normal
+        echo -n ""
+    end
+
+    set_color normal
+    echo -n " "
+end
+
+set fish_color_valid_path
+set fish_pager_color_prefix cyan --bold
+
+fish_add_path /home/cheeze/.ghcup/bin
--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+# Fish
+if [ -d ~/.config/fish ]; then
+	echo "~/.config/fish already exists, skipping..."
+else
+	ln -s ~/dotfiles/fish ~/.config/fish
+	echo "Created a symlink to ~/.config/fish"
+fi
+
 # Kitty
 if [ -d ~/.config/kitty ]; then
 	echo "~/.config/kitty already exists, skipping..."
