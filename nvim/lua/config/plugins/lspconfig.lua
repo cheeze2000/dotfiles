d70356f72343d8e587e3e4a6bba0b909ac132383
cheeze2000 <me@cheeze2000.net>
2022-10-18 21:44:23 +0800

    feat: set up some lsp servers

--- nvim/lua/config/plugins/lspconfig.lua
+++ nvim/lua/config/plugins/lspconfig.lua
@@ -3,4 +3,10 @@ require("mason-lspconfig").setup()
 
 local lspconfig = require("lspconfig")
 
+lspconfig.eslint.setup({})
+lspconfig.hls.setup({})
+lspconfig.intelephense.setup({})
 lspconfig.pyright.setup({})
+lspconfig.tailwindcss.setup({})
+lspconfig.tsserver.setup({})
+lspconfig.volar.setup({})
