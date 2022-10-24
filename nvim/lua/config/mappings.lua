60bc919f36a61d9d7bc761d3402f081d5fed6f09
cheeze2000 <me@cheeze2000.net>
2022-10-24 11:42:03 +0800

    refactor: better neovim mappings abstraction

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,21 +1,38 @@
 local function bind_key(mode, lhs, rhs)
-	vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true })
+	vim.keymap.set(mode, lhs, rhs)
 end
+
+local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
 
 vim.g.mapleader = " "
 
-bind_key("n", "<Leader>w", ":wincmd w<CR>")
+bind_key("n", "<Leader>w", "<Cmd>winc w<CR>")
 
-bind_key("n", "<Leader>c", ":lua require('Comment.api').toggle.linewise.current()<CR>")
-bind_key("v","<Leader>c", ":lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>")
+bind_key("n", "<Leader>c", function ()
+	require("Comment.api").toggle.linewise.current()
+end)
 
-bind_key("n", "<Leader>gd", ":Gitsigns preview_hunk<CR>")
-bind_key("n", "<Leader>gr", ":Gitsigns reset_hunk<CR>")
+bind_key("v", "<Leader>c", function ()
+	vim.api.nvim_feedkeys(esc, "nx", false)
+	require("Comment.api").toggle.linewise(vim.fn.visualmode())
+end)
 
-bind_key("n", "<Leader>a", ":lua vim.lsp.buf.code_action()<CR>")
-bind_key("n", "<Leader>d", ":lua vim.diagnostic.open_float()<CR>")
-bind_key("n", "<Leader>h", ":lua vim.lsp.buf.hover()<CR>")
-bind_key("n", "<Leader>l", ":LspRestart<CR>")
+bind_key("n", "<Leader>gr", "<Cmd>Gitsigns reset_hunk<CR>")
+bind_key("n", "<Leader>gr", "<Cmd>Gitsigns reset_hunk<CR>")
 
-bind_key("n", "<Leader>tf", ":Telescope find_files<CR>")
-bind_key("n", "<Leader>tl", ":Telescope live_grep<CR>")
+bind_key("n", "<Leader>a", function ()
+	vim.lsp.buf.code_action()
+end)
+
+bind_key("n", "<Leader>d", function ()
+	vim.diagnostic.open_float()
+end)
+
+bind_key("n", "<Leader>h", function ()
+	vim.lsp.buf.hover()
+end)
+
+bind_key("n", "<Leader>l", "<Cmd>LspRestart<CR>")
+
+bind_key("n", "<Leader>tf", "<Cmd>Telescope find_files<CR>")
+bind_key("n", "<Leader>tl", "<Cmd>Telescope live_grep<CR>")
