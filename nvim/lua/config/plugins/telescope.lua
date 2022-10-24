d104aeee6c44b5fb060fca4bf58cf9ca36bb61ef
cheeze2000 <me@cheeze2000.net>
2022-10-24 13:04:19 +0800

    feat: more telescope.nvim config

--- nvim/lua/config/plugins/telescope.lua
+++ nvim/lua/config/plugins/telescope.lua
@@ -1,4 +1,12 @@
 require("telescope").setup({
+	defaults = {
+		layout_config = {
+			prompt_position = "top",
+		},
+		sorting_strategy = "ascending",
+		prompt_prefix = " ",
+		selection_caret = " ",
+	},
 	extensions = {
 		["ui-select"] = {
 			require("telescope.themes").get_dropdown({}),
