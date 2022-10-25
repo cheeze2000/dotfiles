621f8865222af42d2f29a7dca9135cf47a4147ed
cheeze2000 <me@cheeze2000.net>
2022-10-25 15:35:22 +0800

    refactor: lazy load hop.nvim on startup

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -3,7 +3,6 @@ local function bind_key(mode, lhs, rhs)
 end
 
 local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
-local hop = require("hop")
 
 vim.g.mapleader = " "
 
@@ -22,7 +21,7 @@ bind_key("n", "<Leader>gd", "<Cmd>Gitsigns preview_hunk<CR>")
 bind_key("n", "<Leader>gr", "<Cmd>Gitsigns reset_hunk<CR>")
 
 bind_key({ "n", "v" }, "<Leader>h", function ()
-	hop.hint_words()
+	require("hop").hint_words()
 end)
 
 bind_key("n", "<Leader>a", function ()
