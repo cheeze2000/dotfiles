667da2993936b735bd3670c8f962f4350283f9cc
cheeze2000 <me@cheeze2000.net>
2024-03-15 21:38:05 +0800

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,32 +2,26 @@ function fish_greeting
 end
 
 function fish_prompt
-    set -l last_status $status
     echo ""
 
-    if [ $last_status -ne 0 ]
-        set_color --bold white -b red
-        echo -n " ! "
-
-        set_color normal
-    end
-
-
     if set -q IN_NIX_SHELL
-        set_color --bold black -b cyan
-        echo -n " 󱄅  "
+        set_color --bold cyan
+        echo -n "󱄅  "
 
         set_color normal
     end
 
-    set_color black -b yellow
-
-    echo -n " $(dirs) "
-    set_color yellow -b normal
-    echo -n ""
+    set_color yellow
+    echo -n "󰉋 "
 
     set_color normal
-    echo -n " "
+    echo " $(dirs)"
+
+    echo -n "󰅂 "
+end
+
+function fish_right_prompt
+    echo $status
 end
 
 alias ns="nix-shell --command fish -p"
